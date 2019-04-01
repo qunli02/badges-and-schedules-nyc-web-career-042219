b3f757ac1ecbbcaf585b30dda1d985f9014105bc
Qun Li Huang <qunli02@gmail.com>
2019-04-01 17:19:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  badge = []
+  array.each do |name|
+    badge.push (badge_maker(name))
+  end
+  return badge
+end
+
+def assign_rooms(array)
+  count = 1
+  room = []
+  array.each do |name|
+    roo = "Hello, #{name}! You'll be assigned to room #{count}!"
+    room.push(roo)
+    count += 1
+  end
+  return room
+end
+
+def printer
+  
+end
