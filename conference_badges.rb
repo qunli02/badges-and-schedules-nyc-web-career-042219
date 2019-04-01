5b25af00de4ff99c663e5d7527635c4716cb1d19
Qun Li Huang <qunli02@gmail.com>
2019-04-01 17:25:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  print batch_badge_creator(array)
-  print assign_rooms(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
